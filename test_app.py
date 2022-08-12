7939432560622359458118146fbe6d90f919e9cc
asim.khan@contify.com <asim.khan@contify.com>
2022-08-12 10:20:08 +0530

    added changes

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@ import logging
 from datetime import datetime
 from functools import lru_cache
 from logging import config as logger_config
-
+import traceback
 import torch
 
 import uvicorn
@@ -78,26 +78,29 @@ async def predict_ner(story: NerText,
     params: story: ArticleText
     Return: Tagged Entities
     """
-
-    story_tuple = []
-    data = story.dict()
-    now = datetime.now()
-    for story_val in data['text']:
-        story_tuple.append(tuple(story_val))
-    if not Doc.has_extension("story_id"):
-        Doc.set_extension("story_id", default=None)
-    doc_tuples = nlp.pipe(story_tuple, as_tuples=True)
-    results = []
-    for doc, context in doc_tuples:
-        doc._.story_id = context["story_id"]
-        prediction = [((ent.start_char, ent.end_char), ent.label_, ent.text)
-                      for ent
-                      in doc.ents]
-        res = {doc._.story_id: prediction, "story_text": doc.text}
-        results.append(res)
-        logger.info(f"Total time taken to process story_id {doc._.story_id} "
-                    f"is : {(datetime.now() - now).total_seconds()}")
-    return results
+    try:
+        story_tuple = []
+        data = story.dict()
+        now = datetime.now()
+        for story_val in data['text']:
+            story_tuple.append(tuple(story_val))
+        if not Doc.has_extension("story_id"):
+            Doc.set_extension("story_id", default=None)
+        doc_tuples = nlp.pipe(story_tuple, as_tuples=True)
+        results = []
+        for doc, context in doc_tuples:
+            doc._.story_id = context["story_id"]
+            prediction = [((ent.start_char, ent.end_char), ent.label_, ent.text)
+                          for ent
+                          in doc.ents]
+            res = {doc._.story_id: prediction, "story_text": doc.text}
+            results.append(res)
+            logger.info(f"Total time taken to process story_id {doc._.story_id} "
+                        f"is : {(datetime.now() - now).total_seconds()}")
+        return results
+    except Exception as err:
+        logger.info(f"Ner Bert: Error occurred for story {story} "
+                    f" Error: {err} , Traceback: {traceback.format_exc()}")
 
 
 if __name__ == '__main__':
