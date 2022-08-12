bbd72510dbc197cfb3a8727bd491de3477098d56
asim.khan@contify.com <asim.khan@contify.com>
2022-08-12 09:25:58 +0530

    changes for pdf autotag

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import traceback
 from datetime import datetime
 from functools import lru_cache
 from logging import config as logger_config
-import traceback
 
 import torch
 
@@ -81,26 +80,24 @@ async def predict_ner(story: NerText,
     params: story: ArticleText
     Return: Tagged Entities
     """
-    try:
-        story_tuple = []
-        data = story.dict()
-        now = datetime.now()
-        for story_val in data['text']:
-            story_tuple.append(tuple(story_val))
-        if not Doc.has_extension("story_id"):
-            Doc.set_extension("story_id", default=None)
-        doc_tuples = nlp.pipe(story_tuple, as_tuples=True)
-        for doc, context in doc_tuples:
-            doc._.story_id = context["story_id"]
-            prediction = [((ent.start_char, ent.end_char), ent.label_, ent.text) for ent
-                  in doc.ents]
-            res = {doc._.story_id: prediction, "story_text": doc.text}
-            logger.info(f"Total time taken to process story_id {doc._.story_id} "
-                        f"is : {(datetime.now() - now).total_seconds()}")
-            return res
-    except Exception as err:
-        logger.info(f"Ner Bert: Error occurred for story {story} "
-                    f" Error: {err} , Traceback: {traceback.format_exc()}")
+    story_tuple = []
+    data = story.dict()
+    now = datetime.now()
+    for story_val in data['text']:
+        story_tuple.append(tuple(story_val))
+    if not Doc.has_extension("story_id"):
+        Doc.set_extension("story_id", default=None)
+    doc_tuples = nlp.pipe(story_tuple, as_tuples=True)
+    results = []
+    for doc, context in doc_tuples:
+        doc._.story_id = context["story_id"]
+        prediction = [((ent.start_char, ent.end_char), ent.label_, ent.text) for ent
+              in doc.ents]
+        res = {doc._.story_id: prediction, "story_text": doc.text}
+        results.append(res)
+        logger.info(f"Total time taken to process story_id {doc._.story_id} "
+                    f"is : {(datetime.now() - now).total_seconds()}")
+    return results
 
 @app.post('/predict/test_ner/')
 async def predict_ner(story: NerText,
